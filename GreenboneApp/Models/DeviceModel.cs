a3e644d8d9a416f88416ef362158acbcbe40974b
mspundflasche <mspundfl@protonmail.com>
2022-05-21 11:00:10 +0200

    Add API Project and computer Model

---
+++ GreenboneApp/Models/DeviceModel.cs
@@ -0,0 +1,11 @@
+namespace GreenboneApp.Models
+{
+    public class DeviceModel
+    {
+        public string MACAddress { get; set; }
+        public string DeviceName { get; set; }
+        public string IPAddress { get; set; }
+        public string Abbrevation { get; set; }
+        public string Description { get; set; }
+    }
+}
