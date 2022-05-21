14ade58ad2aa90c84195e24fef12c6ca0120fd9a
Martin Spundflasche <mspundfl@protonmail.com>
2022-05-21 13:48:00 +0200

    Add Sample Controller and Database Access Class

---
+++ GreenboneApp/Controllers/DeviceController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using GreenboneApp.Models;
+using GreenboneApp.Data;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace GreenboneApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DeviceController : ControllerBase
+    {
+        const string ConnectionString = "server=127.0.0.1;uid=root;pwd=12345;database=test";
+
+        // GET: api/<DeviceController>
+        [HttpGet]
+        public IEnumerable<DeviceModel> Get()
+        {
+            DBAccess db = new DBAccess();
+            return db.LoadDataFromDatabase<DeviceModel, dynamic>("SELECT * FROM DeviceTable", new { }, ConnectionString);
+        }
+
+        // GET api/<DeviceController>/5
+        [HttpGet("{id}")]
+        public List<DeviceModel> Get(string macAddress)
+        {
+            DBAccess db = new DBAccess();
+            return db.LoadDataFromDatabase<DeviceModel, dynamic>("SELECT * WHERE MACAddress=" + macAddress + " FROM DeviceTable", new { }, ConnectionString);
+        }
+
+        // POST api/<DeviceController>
+        [HttpPost]
+        public void Post([FromBody] DeviceModel device)
+        {
+            DBAccess dB = new DBAccess();
+
+            List<DeviceModel> deviceModel = dB.LoadDataFromDatabase<DeviceModel, dynamic>("SELECT * WHERE MACAddress=" + device.MACAddress + " FROM DeviceTable", new { }, ConnectionString);
+
+            if (deviceModel is null)
+            {
+                string sql = "INSERT INTO DeviceTable VALUES (" + device.MACAddress + ", " + device.DeviceName + ", " + device.IPAddress + ", " + device.Abbrevation + "," + device.Description + ")";
+                dB.SaveDataInDatabase(sql, new { }, ConnectionString);
+            }
+        }
+
+        // PUT api/<DeviceController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<DeviceController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ GreenboneApp/Data/DBAccess.cs
@@ -0,0 +1,31 @@
+﻿using MySql.Data;
+using System.Threading.Tasks;
+using Dapper;
+using System.Data;
+using MySql.Data.MySqlClient;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GreenboneApp.Data
+{
+    public class DBAccess
+    {
+        public void SaveDataInDatabase<T>(string _sql, T _parameters, string _connectionstring)
+        {
+            using (IDbConnection connection = new MySqlConnection(_connectionstring))
+            {
+                connection.Execute(_sql, _parameters);
+            }
+        }
+
+        public List<T> LoadDataFromDatabase<T, U>(string _sql, U _parameters, string _connectionstring)
+        {
+            using(IDbConnection connection = new MySqlConnection(_connectionstring))
+            {
+                var data = connection.Query<T>(_sql, _parameters);
+
+                return data.ToList();
+            }
+        }
+    }
+}
